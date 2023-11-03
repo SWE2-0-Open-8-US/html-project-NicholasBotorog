cbef2cb4049d125ef2c2b50bd27798e01ff97537
Nicholas Botorog <nicholasbotorog@gmail.com>
2023-11-03 15:41:48 -0400

    ditched the idea of a slider

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const test = document.querySelector('p')
-test.addEventListener('click', function(){
-    console.log('Test')
+const slide = document.querySelectorAll('.slide')
+const btnNext = document.querySelector('#next')
+const btnPrev = document.querySelector('#prev')
+
+let count = 0 
+
+slide.forEach((slide, index)=>{
+    slide.computedStyleMap.left = `${index * 100}%`
 })
+
+const next = () => { 
+    counter++ 
+    slideContent()
+}
+
+const previous = () => { 
+    counter--
+    slideContent()
+}
+
+const slideContent = () => { 
+    slides.forEach(slide =>{
+        slide.style.transform =`transalteX(-${counter * 100}%)`
+    })
+}
+
+// btnNext.addEventListener('click', next)
+// btnPrev.addEventListener('click', previous)
